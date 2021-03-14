69195379703b1b813e1ae46d700aa748d59c12e8
2mkat <misyura200@gmail.com>
2021-03-14 13:06:20 +0700

    working table

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include  <clocale>
+#include <Windows.h>
 
 /*
 class node{
@@ -9,15 +11,39 @@ private:
 
 };*/
 
+void creat_table (int &table, std::string str){
+    std::cout << str << '\n';
+    int* point = &table;
+    for(auto x : str){
+        point[x]++;
+    }
+    //std::cout << table[4] << '\n';
+}
+
 int main() {
 
-    setlocale( LC_ALL, "rus");
-    char buff[50];
+    setlocale(LC_ALL,"Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string file_result;
     std::ifstream file("text.txt");
 
-    file >> buff;
-    //std::cout << buff << std::endl;
+    if (!file){
+        std::cerr << "Uh oh, Text.txt could not be opened for reading!" << '\n';
+        exit(1);
+    }
 
+    int table[255] = {0};
+
+    while(!file.eof()) {
+        file >> file_result;
+        creat_table(*table, file_result);
+    }
+
+    for(int i = 0; i < 255; ++i){
+        std::cout << i << ": " << table[i] << '\n';
+    }
 
     return 0;
 }
