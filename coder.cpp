308de1994cc7b6e1a35f19d5b07fc04a05731d6b
2mkat <misyura200@gmail.com>
2021-04-11 13:10:20 +0700

    add encode file

--- coder.cpp
+++ coder.cpp
@@ -13,11 +13,11 @@ class Node{
 public:
     char s_;    ///symbol
     int key_;   /// amount symbols
-    Node *left_, *right_;   ///pointers on left and right
+    Node *left_, *right_;   ///pointers on left_ and right_
 
     Node(){
-        //key_ = 0;
-        //s_ = ' ';
+        key_ = 0;
+        s_ = ' ';
         left_ = nullptr;
         right_ = nullptr;
     }   /// default constructor
@@ -35,10 +35,6 @@ public:
     ~Node(){
         delete[] left_;
         delete[] right_;
-    }
-    friend std::ostream& operator<<(std::ostream& out, Node& c1){
-        out << c1.s_ << " " << c1.key_ << '\n';
-        return out;
     }
 };
 bool comp (const Node *c1, const Node *c2){
@@ -72,7 +68,6 @@ int main() {
         std::cerr << "Uh oh, code.txt could not be opened for writing!" << '\n';
         exit(1);
     }
-
     std::ifstream file("text.txt", std::ios::out | std::ios::binary);
     if (!file){
         std::cerr << "Uh oh, Text.txt could not be opened for reading!" << '\n';
@@ -81,32 +76,28 @@ int main() {
 
     std::map<char, int> table;
     std::map<char, int>:: iterator it;
-
     char s;
-    /*while(file >> s) {
-        table[s]++;
-    } this is not work ?*/
     while(!file.eof()) {
         s = file.get();
         table[s]++;
     }
+
     int count = 0;
     for (it = table.begin(); it != table.end(); it++){
         if (it->second != 0){
-            ++count;
+            count += 40;
         }
     }
-    //file_res << count;
     file_res.write((char*)(&count), sizeof(count));
+
     for(it = table.begin(); it != table.end(); ++it){
-        //file_res << it->first << it->second;
         file_res.write((char*)(&it->first), sizeof(it->first));
         file_res.write((char*)(&table[it->first]), sizeof(table[it->first]));
     }
 
     std::list<Node*> l;
     for(it = table.begin(); it != table.end(); ++it){
-        Node *p = new Node(it->first, it->second);  ///char s = it->first nt key = it->second
+        Node *p = new Node(it->first, it->second);  ///char s_ = it->first nt key_ = it->second
         l.push_back(p);
     }
 
